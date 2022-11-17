3ef2d9bfc82025062c8d84a102c8b345f9297c9c
Mikayel Ghambaryan <mikayel-sta@yandex.ru>
2022-11-18 01:39:21 +0400

    Update

--- class.cpp
+++ class.cpp
@@ -1,38 +1,62 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+
+
 
 class CoffeeMachine
     {
 
     public:
+		int m_nCount;
         string m_sProducts_name;
-        int m_nCount;
-        const char* m_aProducts_Names;
-        const char* m_aProducts_Prises;
+        int m_nPrice;
+        string* m_aProducts_Names;
+        int* m_aProducts_Prises;
     public:
-        CoffeeMachine(string sProducts_name)
+
+        CoffeeMachine()
         : m_sProducts_name("prise name" )
-         , m_nCount(0)
-        , m_aProducts_Names(new char [m_nCount])    
-        , m_aProducts_Prises(new char [m_nCount])    
+        , m_nPrice(0)
+        , m_aProducts_Names(new string [m_nCount])    
+        , m_aProducts_Prises(new int [m_nCount])
+		, m_nCount(0)    
         {
-        cout<<" CoffeeMachine()"<<endl;
+
         }
+
+        CoffeeMachine (string sProd_name, int Price, int Count)
+        : m_sProducts_name ( "prise name" )
+        , m_nPrice(0)
+        , m_aProducts_Names (new string [m_nCount])    
+        , m_aProducts_Prises (new int [m_nCount])    
+		, m_nCount(0)    
+        	{
+			m_nCount = Count;
+			m_nPrice = Price;
+			m_sProducts_name = sProd_name;
+			m_aProducts_Prises [ m_nCount ] = m_nPrice;
+        	}
+
 void printPriceList()
         {
-        cout<<" "<<endl;
+		cout << m_sProducts_name <<" "<< m_nPrice <<" "<< endl;
+		cout << m_aProducts_Prises [ m_nCount ] << endl;
         }
+
         ~CoffeeMachine()
-        {
-        delete [] m_aProducts_Names;
-        delete [] m_aProducts_Prises;
-        }
+        	{
+        	delete [] m_aProducts_Names;
+        	delete [] m_aProducts_Prises;
+       	 	}
     };
 int main(){
-    int x=3;
-    int y=4;
-CoffeeMachine st "name";
-arrCoffe[4].m_nCount = 5;
+    
+CoffeeMachine coffe( "Espreso", 200, 5);
+CoffeeMachine coffe1;
+coffe1.printPriceList();
+coffe.printPriceList();
 
 
 }
